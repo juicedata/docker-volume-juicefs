374868480669a89afcaae4d413ca2edd2a68402f
Xuhui zhang <xuhui@juicedata.io>
2026-04-29 10:44:00 +0800

    fix remove of never-mounted volumes

--- main.go
+++ main.go
@@ -339,7 +339,7 @@ func (d *jfsDriver) Remove(r *volume.RemoveRequest) error {
 		return logError("volume %s is in use", r.Name)
 	}
 
-	if err := os.Remove(v.Mountpoint); err != nil {
+	if err := os.Remove(v.Mountpoint); err != nil && !os.IsNotExist(err) {
 		return logError(err.Error())
 	}
 

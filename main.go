94d04619775e8b1da286972afe50b4f68ecc54d4
Frice Xu <39143307+Xuyuchao-juice@users.noreply.github.com>
2026-08-17 10:13:08 +0800

    Fix concurrency and reference counting in Mount/Unmount (#73)

--- main.go
+++ main.go
@@ -471,14 +471,18 @@ func (d *jfsDriver) Path(r *volume.PathRequest) (*volume.PathResponse, error) {
 func (d *jfsDriver) Mount(r *volume.MountRequest) (*volume.MountResponse, error) {
 	logrus.WithField("method", "mount").Debugf("%#v", r)
 
+	d.Lock()
+	defer d.Unlock()
+
 	v, ok := d.volumes[r.Name]
 	if !ok {
 		return &volume.MountResponse{}, logError("volume %s not found", r.Name)
 	}
 
-	err := mountVolume(v)
-	if err != nil {
-		return &volume.MountResponse{}, logError("failed to mount %s: %s", r.Name, err)
+	if v.connections == 0 {
+		if err := mountVolume(v); err != nil {
+			return &volume.MountResponse{}, logError("failed to mount %s: %s", r.Name, err)
+		}
 	}
 
 	v.connections++
@@ -488,16 +492,23 @@ func (d *jfsDriver) Mount(r *volume.MountRequest) (*volume.MountResponse, error)
 func (d *jfsDriver) Unmount(r *volume.UnmountRequest) error {
 	logrus.WithField("method", "umount").Debugf("%#v", r)
 
+	d.Lock()
+	defer d.Unlock()
+
 	v, ok := d.volumes[r.Name]
 	if !ok {
 		return logError("volume %s not found", r.Name)
 	}
 
-	if err := umountVolume(v); err != nil {
-		return logError("failed to umount %s: %s", r.Name, err)
+	if v.connections <= 1 {
+		if err := umountVolume(v); err != nil {
+			return logError("failed to umount %s: %s", r.Name, err)
+		}
+		v.connections = 0
+	} else {
+		v.connections--
 	}
 
-	v.connections--
 	return nil
 }
 
